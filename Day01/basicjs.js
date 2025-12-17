2792565e9a1c1f6471c131b71e7917072814dffb
VijaydossDevadoss <vijaydoss92@gmail.com>
2025-12-17 12:26:26 +0530

    Add files via upload
    Day1

---
+++ Day01/basicjs.js
@@ -0,0 +1,11 @@
+var testerName = "Vijay"
+var organizationName = "TestLeaf"
+var contactNumber = 7401566628
+var isAutomationTester = false
+var hasPlaywrightExperience
+
+console.log(testerName + " " + typeof testerName)
+console.log(organizationName + " " + typeof organizationName)
+console.log(contactNumber + " " + typeof contactNumber)
+console.log(isAutomationTester + " " + typeof isAutomationTester)
+console.log(hasPlaywrightExperience + " " + typeof hasPlaywrightExperience)
