a326fb615e3d03ac657617e656344b6d909fbe01
VijaydossDevadoss <vijaydoss92@gmail.com>
2025-12-17 12:27:03 +0530

    Add files via upload
    Day2

---
+++ Day02/conditions.js
@@ -0,0 +1,16 @@
+let mark =75
+if (mark>=90)
+{
+    console.log("A Grade")
+}
+else if (mark>=80)
+{
+    console.log("B Grade")
+}
+else if (mark>=70)
+{
+    console.log("C Grade")
+}
+else{
+    console.log("Fail")
+}
---
+++ Day02/scoping.js
@@ -0,0 +1,14 @@
+var browserversion = 11
+{
+var browsername = "Chrome"
+let browserversion = 10
+const loadurl = "www.google.com"
+
+console.log ("inside the block:"+ browsername)
+console.log ("inside the block:" + browserversion)
+console.log ("inside the block:" + loadurl)
+}
+
+console.log("outside the block:" + browsername)
+console.log("outside the block:"+ browserversion)
+console.log("outside the block:"+ loadurl)
